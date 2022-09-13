19f329319edb6910fba05936700225a80ac0cb5e
Hankerman <stalker-2204@mail.ru>
2022-09-13 22:53:00 +0300

    Добавьте файлы проекта.

---
+++ HW6/Task1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW6
+{
+    public class Money
+    {
+
+        int whole;
+        int fractional;
+        string symbol;
+        int Whole { get; set; }
+        int Fractional { get; set; }
+        string Symbol { get; set; }
+
+        public Money()
+        {
+            whole = 0;
+            fractional = 0;
+            symbol = "P";
+        }
+
+        public Money(string symbol)
+        {
+            whole = 0;
+            fractional = 0;
+            Symbol = symbol;
+        }
+
+        public Money(int whole, int fractional, string symbol)
+        {
+            Whole = whole;
+            Fractional = fractional;
+            Symbol = symbol;            
+        }
+
+        public override string ToString()
+        {
+            return $"{whole},{Fractional} {symbol}";
+        }
+
+    }
+}
---
+++ HW6/Task2.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW6
+{
+    public abstract class Device
+    {
+
+        public abstract string Song();
+        public abstract string Show();
+        public abstract string Desc();
+
+    }
+
+    public class Teapot : Device
+    {
+        public override string Desc()
+        {
+            return "Кипятит воду";
+        }
+
+        public override string Show()
+        {
+            return "Чайник";
+        }
+
+        public override string Song()
+        {
+            return "\"Свистит от закипания\"";
+        }
+    }
+
+    public class MicrowaveOven : Device
+    {
+        public override string Desc()
+        {
+            return "Греет еду";
+        }
+
+        public override string Show()
+        {
+            return "Микроволновая печь";
+        }
+
+        public override string Song()
+        {
+            return "Дзьнь";
+        }
+    }
+
+    public class Car : Device
+    {
+        public override string Desc()
+        {
+            return "Возит людей";
+        }
+
+        public override string Show()
+        {
+            return "Автомобиль";
+        }
+
+        public override string Song()
+        {
+            return "Бип - бип блин";
+        }
+    }
+
+    public class Steamboat : Device
+    {
+        public override string Desc()
+        {
+            return "Двигается по воде и плохо по земле";
+        }
+
+        public override string Show()
+        {
+            return "Пароход";
+        }
+
+        public override string Song()
+        {
+            return "Ту - ту";
+        }
+    }
+
+}
